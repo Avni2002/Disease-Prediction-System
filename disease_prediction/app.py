f7d8a02880423be5a486e4fcebf56b42212b120e
Rituparna Meher <meherrituparna@gmail.com>
2025-02-05 11:06:31 +0530

    Three Disease comitted

--- disease_prediction/app.py
+++ disease_prediction/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -22,27 +23,30 @@ def home():
 @app.route('/predict/heart', methods=['POST'])
 def predict_heart_disease():
     data = request.get_json(force=True)
-    features = np.array([list(data.values())])
+    column_names = ['age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal']
+    features = pd.DataFrame([list(data.values())], columns=column_names)
     prediction = heart_model.predict(features)
-    result = "Disease" if prediction[0] == 1 else "No Disease"
+    result = "disease" if prediction[0] == 1 else "no-disease"
     return jsonify({"prediction": result})
 
 # Route for diabetes prediction
 @app.route('/predict/diabetes', methods=['POST'])
 def predict_diabetes():
     data = request.get_json(force=True)
-    features = np.array([list(data.values())])
+    column_names = ['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age']
+    features = pd.DataFrame([list(data.values())], columns=column_names)
     prediction = diabetes_model.predict(features)
-    result = "Disease" if prediction[0] == 1 else "No Disease"
+    result = "disease" if prediction[0] == 1 else "no-disease"
     return jsonify({"prediction": result})
 
 # Route for liver disease prediction
 @app.route('/predict/liver', methods=['POST'])
 def predict_liver_disease():
     data = request.get_json(force=True)
-    features = np.array([list(data.values())])
+    column_names=['Age','Gender','Total_Bilirubin','Direct_Bilirubin','Alkaline_Phosphotase','Alamine_Aminotransferase','Aspartate_Aminotransferase','Total_Protiens','Albumin','Albumin_and_Globulin_Ratio']
+    features = pd.DataFrame([list(data.values())], columns=column_names)
     prediction = liver_model.predict(features)
-    result = "Disease" if prediction[0] == 1 else "No Disease"
+    result = "disease" if prediction[0] == 1 else "no-disease"
     return jsonify({"prediction": result})
 
 if __name__ == "__main__":
